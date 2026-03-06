1e681bd187b13d51f3a5c22bd186a5fa87ccc6be
Vincent Stridh <vincent.stridh@elev.ga.ntig.se>
2026-03-06 16:48:00 +0100

    made everything work

--- sketch_ownproject2/sketch_ownproject2.ino
+++ sketch_ownproject2/sketch_ownproject2.ino
@@ -1,16 +1,38 @@
 /*  
 * 
 */  
-int relay = 8;
-int ir = 7;
+//Constants
+#include <IRremote.h>
+
+const int relayPin = 8;
+const int RECV_PIN = 11;
+
+//Variables
+int IRValue;
+
+//Objects
+IRrecv irrecv(RECV_PIN);
+decode_results  results;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  irrecv.enableIRIn();
+  pinMode(relayPin, OUTPUT);
+  pinMode(RECV_PIN, INPUT);
+  Serial.begin(9600);
+  digitalWrite(relayPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if ir =
+  if (irrecv.decode(&results))  {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive  the next value
+    digitalWrite(relayPin, HIGH);
+    Serial.println("HIGH");
+  }
+  else {
+    digitalWrite(relayPin, LOW);
+    Serial.println("LOW");
+  }
+  delay(100);
 }
 
